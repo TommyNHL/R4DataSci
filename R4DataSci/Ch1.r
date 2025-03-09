371daadd36f3c1b9fbd79b1308b45eb66295f0e0
TommyNHL <research.tommyn.lok@gmail.com>
2025-03-09 23:29:05 +0800

    ch1

---
+++ R4DataSci/Ch1.r
@@ -0,0 +1,8 @@
+install.packages("tidyverse")
+tidyverse_update()
+library(tidyverse)
+
+print("hello world!")
+print(c("hello", " ", "world!"))
+
+install.packages(c("nycflights13", "gapminder", "Lahman"))

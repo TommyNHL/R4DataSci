37db0d3afb630a7f1fe2492ce4f4e781bf284f6c
TommyNHL <research.tommyn.lok@gmail.com>
2025-03-12 18:26:32 +0800

    ch1

--- R4DataSci/Ch1.r
+++ R4DataSci/Ch1.r
@@ -4,6 +4,8 @@ ggplot2::ggplot()
 # mpg df
 ggplot2::mpg  # manufactuer/chr; model/chr; displ/dbl; year/int; cyl/int; trans/chr; drv/chr; cty/int; hwy/int; fl/chr; class/chr
 
+ggplot(data = mpg)
+
 ggplot(data = mpg) + 
     geom_point(mapping = aes(x = displ, y = hwy))
 

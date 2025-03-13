23024814c26b4bdac0ae7a63d64db463f32a251d
TommyNHL <research.tommyn.lok@gmail.com>
2025-03-14 00:09:57 +0800

    ch1_barPlots

--- Ch1_barPlot.r
+++ Ch1_barPlot.r
@@ -1,5 +1,16 @@
 library(tidyverse)
 ggplot2::ggplot()
 
+## diamonds df
+ggplot2::diamonds  # carat/dbl; cut/ord; color/ord; clarity/ord; depth/dbl; table/dbl; price/int; x/dbl; y/dbl; z/dbl
+
 # Statistical Transformations
 ggplot(data = diamonds)
+
+## geom_bar()
+ggplot(data = diamonds) + 
+    geom_bar(mapping = aes(x = cut))  # calls
+## stat_count()
+ggplot(data = diamonds) + 
+    stat_count(mapping = aes(x = cut))
+

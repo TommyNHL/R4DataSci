f48d7bd53f0f5524f790e955316b61236b4d4169
TommyNHL <research.tommyn.lok@gmail.com>
2025-03-14 21:47:35 +0800

    ch2_basics

--- Ch2_basics.r
+++ Ch2_basics.r
@@ -9,5 +9,7 @@ x  # 12
 r = 2 ^ 3
 r  # 8
 
-# Calling Fun
-
+# Calling Bulti-in Func
+seq(0, 10)  # range(0, 11), collect(0:10)
+seq(0, 10, length.out = 5)  # collect(0:2.5:10)
+(y <- seq(0, 10, length.out = 5))  # assignment + print

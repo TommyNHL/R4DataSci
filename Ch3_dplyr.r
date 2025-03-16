6f322e27691294992e9c644d7cb9fdc29cd4abda
TommyNHL <research.tommyn.lok@gmail.com>
2025-03-16 12:54:47 +0800

    ch3_dplyr

--- Ch3_dplyr.r
+++ Ch3_dplyr.r
@@ -38,7 +38,7 @@ filter(flights, month %in% c(11, 12))
 filter(flights, !(arr_delay > 120 | dep_delay > 120))  # or
 filter(flights, arr_delay <= 120, dep_delay <= 120)
 
-## range()
+## arrange(), sort by
 arrange(flights, year, month, day)  # ascending order
 arrange(flights, desc(arr_delay))  # descending order
 df <- tibble(x = c(5, 2, NA))
@@ -46,4 +46,38 @@ df <- tibble(x = c(5, 2, NA))
 arrange(df, x)  # NA is sorted at the end
 arrange(df, desc(x))  # NA is sorted at the end
 
+## select()
+select(flights, year, month, day)  # select col by name
+select(flights, year:day)  # select col between year and day (inclusive)
+select(flights, -(year:day))  # select all col except those from year to day
+select(flights, starts_with("sched"))
+select(flights, ends_with("time"))
+select(flights, contains("dep"), contains("arr"))
+select(flights, matches("(.)\\1"))  # any variables contain repeated char
+df <- tibble(x1 = c(1, 2, 3), x2 = c(2, 3, 4), x3 = c(3, 4, 5), x4 = c(4, 5, 6))
+select(df, num_range("x", 1:3))  # matches x1, x2, x3, so x4 is unselected
+select(flights, time_hour, air_time, everything())  # move col to the start of the df
+
+## rename(), a variant of select()
+rename(flights, tail_num = tailnum)  # new_name = old_name
+
+## mutate(), the new variables are kept at the end of df
+flights_sml <- select(flights, 
+    year:day, 
+    ends_with("delay"), 
+    distance, air_time)
+View(flights_sml)
+mutate(flights_sml, 
+    gain = arr_delay - dep_delay, 
+    speed = distance / air_time * 60)
+mutate(flights_sml, 
+    gain = arr_delay - dep_delay, 
+    hours = air_time / 60, 
+    gain_per_hour = gain / hours)
+
+## transmute(), only keep the new variables
+transmute(flights, 
+    gain = arr_delay - dep_delay, 
+    hours = air_time / 60, 
+    gain_per_hour = gain / hours)
 

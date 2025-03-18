a6adaf756d6c2a706764734b23b0e7406cd773b0
TommyNHL <research.tommyn.lok@gmail.com>
2025-03-18 11:20:30 +0800

    RNA-seq

--- RNASeqWithBioconductorInR.r
+++ RNASeqWithBioconductorInR.r
@@ -2,7 +2,7 @@ install.packages(c("RColorBrewer", "pheatmap", "tidyverse"))
 
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install(version = "3.14")  # up-tp-date 3.20
+BiocManager::install(version = "3.14")  # up-tp-date 3.20, AIMs 3.18, laptop 3.14
 
 BiocManager::install("DESeq2")
 
